5a12d3fc2bfc7be673a3e2ff121ad2eedd575626
罗好园 <zealot2010@hotmail.com>
2016-01-26 23:34:49 +0800

    case 4 fault

--- PAT/1003.cpp
+++ PAT/1003.cpp
@@ -3,83 +3,60 @@
 #include <queue>
 using namespace std;
 
-struct adj_city
-{
-	int name;
-	int length;
-};
-
-struct city
-{
-	vector<adj_city> adj;
-	int dist;
-	int name;
-	int team;
-	bool known;
-	bool operator < (city const& _A) const
-	{
-		if (dist > _A.dist) 
-			return true;
-		else return false;
-	}
-};
-
-vector<city> c;
-priority_queue<city> q;
-vector<int>n_team;
 int n, l, s, e;
-int c1, c2, w12;
-int team, path, minl;
-bool findresult = false;
-city v;
+int ls, le, lv;
 
+vector<int> Team;
 
-int main()
+struct adj_list
 {
-	cin >> n >> l >> s >> e;
-	n_team.resize(n);
-	c.resize(n);
-	for (int i = 0; i < n; ++i)
+	int city;
+	int len;
+};
+
+struct node
+{
+	int city;
+	int dist;
+	int team;
+	vector<adj_list> adj;
+	bool closed;
+};
+
+bool operator < (const node &p1, const node & p2)
+{
+	return p1.dist > p2.dist;
+}
+
+priority_queue<node> opened;
+
+bool findresult = false;
+int minlen = 0;
+int path, team;
+node v;
+vector<node> c;
+int c1, c2, d;
+adj_list a, b;
+
+void slove()
+{
+	while (!opened.empty())
 	{
-		v.dist = INT32_MAX;
-		v.name = i;
-		v.team = 0;
-		v.known = false;
-		c[i] = v;
-		cin >> n_team[i];
-	}
-	for (int i = 0; i < l; ++i)
-	{
-		cin >> c1 >> c2 >> w12;
-		adj_city a, b;
-		a.name = c2;
-		a.length = w12;
-		b.name = c1;
-		b.length = w12;
-		c[c1].adj.push_back(a);
-		c[c2].adj.push_back(b);
-	}
-	minl = 0;
-	c[s].dist = 0;
-	c[s].team = n_team[s];
-	q.push(c[s]);
-	while (!q.empty())
-	{
-		v = q.top();
-		q.pop();
-		c1 = v.name;
-		if (findresult && v.dist > minl)
+		v = opened.top();
+		opened.pop();
+		c1 = v.city;
+		if (findresult && v.dist > minlen)
 			continue;
-		if (v.name == e)
+		if (v.city == e)
 		{
-			if (!findresult || v.dist < minl)
+			if (v.dist < minlen || !findresult)
 			{
 				findresult = true;
 				path = 1;
-				minl = v.dist;
+				minlen = v.dist;
 				team = v.team;
 			}
-			else if (v.dist == minl)
+			else if (v.dist == minlen)
 			{
 				++path;
 				if (v.team > team)
@@ -87,19 +64,50 @@ int main()
 			}
 			continue;
 		}
-		c[c1].known = true;
-		for (int i = 0; i < (int)v.adj.size(); ++i)
+		c[c1].closed = true;
+		for (unsigned i = 0; i < v.adj.size(); ++i)
 		{
-			c2 = v.adj[i].name;
-			w12 = v.adj[i].length;
-			if (!c[c2].known)
+			c2 = c[c1].adj[i].city;
+			d = c[c1].adj[i].len;
+			if (!c[c2].closed)
 			{
-				c[c2].dist = c[c1].dist + w12;
-				c[c2].team = c[c1].team + n_team[c2];
-				q.push(c[c2]);
+				c[c2].dist = c[c1].dist + d;
+				c[c2].team = c[c1].team + Team[c2];
+				opened.push(c[c2]);
 			}
 		}
 	}
-	cout << path << " " << team;
+}
+
+int main()
+{
+	cin >> n >> l >> s >> e;
+	for (int i = 0; i < n; ++i)
+	{
+		v.dist = INT32_MAX;
+		v.city = i;
+		v.team = 0;
+		v.closed = false;
+		c.push_back(v);
+		cin >> team;
+		Team.push_back(team);
+	}
+	for (int i = 0; i < l; ++i)
+	{
+		cin >> ls >> le >> lv;
+		a.city = le;
+		b.city = ls;
+		a.len = b.len = lv;
+		c[ls].adj.push_back(a);
+		c[le].adj.push_back(b);
+	}
+	c[s].dist = 0;;
+	c[s].team = Team[s];
+	opened.push(c[s]);
+	slove();
+	cout << path << ' ' << team;
 	return 0;
 }
+
+
+
